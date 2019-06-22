0b3f5b8aec10282e5c54954318840647113fcb54
jaeju.jang <jjj0611@gmail.com>
2019-06-22 23:11:15 +0900

    [EDIT] JSX 내부에서 if문을 사용할 수 없기 때문에 조건부 연산자를 사용하는 코드를 작성하였습니다.

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,13 +1,17 @@
-import React, {Component, Fragment} from 'react';
+import React, {Component} from 'react';
 import logo, { ReactComponent } from './logo.svg';
 import './App.css';
 
 function App() {
   const text = '당신은 어썸한가요?';
+  const condition = true;
   return (
     <div>
       <h1>리액트 안녕!</h1>
       <h2>{text}</h2>
+      {
+        condition ? '참' : '거짓'
+      }
     </div>
   );
 }
