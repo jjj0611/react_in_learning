9a3629bd88f59953e19e36dfe1bcc35ca6f616d7
jaeju.jang <jjj0611@gmail.com>
2019-06-22 23:14:46 +0900

    [EDIT] && 연산자를 이용한 조건부 렌더링을 학습하는 코드를 작성하였습니다.

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -4,13 +4,13 @@ import './App.css';
 
 function App() {
   const text = '당신은 어썸한가요?';
-  const condition = true;
+  const condition = false;
   return (
     <div>
       <h1>리액트 안녕!</h1>
       <h2>{text}</h2>
       {
-        condition ? '참' : '거짓'
+        condition && '보여주세요'
       }
     </div>
   );
