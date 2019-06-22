7f6dc1108b4143ae60134964be04d28a564328d0
jaeju.jang <jjj0611@gmail.com>
2019-06-22 23:51:35 +0900

    [EDIT] 주석을 처리하는 방법에 대해 학습하는 코드를 작성하였습니다.

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -16,11 +16,19 @@ function App() {
   };
   return (
     <div className="my-div">
-      <h1>리액트 안녕!</h1>
-      <h2>{text}</h2>
-      { condition && '보여주세요' }
-      <div style={style}></div>
-    </div>
+    {/* 요소 밖에서는 이렇게 작성해요. */}
+    <h1>리액트 안녕!</h1>
+    <h2>{text}</h2>
+    { condition && '보여주세요' }
+    <div
+            style={style}
+    // self-closed 태그에서만 작동하는 주석
+    // 마지막에 />가 꼭 새줄에 있어야 한다.
+    /* 이렇게 작성할 수도 있고요. */
+    />
+    // 여기 쓰는건 그대로 렌더링 된다.
+    /* 여기에선 주석을 아예 쓸 수 없는 것이다 */
+</div>
   );
 }
 
