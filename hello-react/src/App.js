8321f13c6f77758909d0f9c19972db18d0ed1171
jaeju.jang <jjj0611@gmail.com>
2019-06-25 23:44:48 +0900

    [ADD] 함수형 컴포넌트를 선언하는 Hello.js 코드를 만들었습니다.

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
+import Hello from './Hello'
 
 class App extends Component {
   render() {
     return (
       <div>
-        
+        <Hello name='재주' />
       </div>
     )
   }
