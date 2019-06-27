a1173a036fece230174fb4bf155b463fc5f25b11
jaeju.jang <jjj0611@gmail.com>
2019-06-27 21:29:59 +0900

    [EDIT] scss파일을 import시 파일 명을 입력할 때 PageTemplate.scss가 아니라 PageTemplate.module.scss처럼 입력해야해서 수정하였습니다.

--- todo-list/src/components/PageTemplate/PageTemplate.tsx
+++ todo-list/src/components/PageTemplate/PageTemplate.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './PageTemplate.scss';
+import styles from './PageTemplate.module.scss';
 import classNames from 'classnames/bind';
 
 const cx = classNames.bind(styles);
