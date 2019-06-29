5e32f11fabe933a966667b7c6d67efdd5621d54d
jaeju.jang <jjj0611@gmail.com>
2019-06-29 16:46:27 +0900

    [ADD] TodoList 컴포넌트를 생성하였습니다.

---
+++ todo-list/src/components/TodoList/TodoList.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import TodoItem from '../TodoItem';
+
+class TodoList extends React.Component {
+    render() {
+        return (
+            <div>
+                <TodoItem done>리액트 공부하기</TodoItem>
+                <TodoItem>컴포넌트 스타일링 해보기</TodoItem>
+            </div>
+        )
+    }
+}
