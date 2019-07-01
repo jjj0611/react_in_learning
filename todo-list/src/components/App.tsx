53c1e0f992a5b49b1a0535c7d240db5e0275c270
jaeju.jang <jjj0611@gmail.com>
2019-07-01 20:42:53 +0900

    [ADD] App.tsx에서 TodoInput 컴포넌트의 input 상태를 관리하기 위해 state를 정의하고, 이벤트를 처리할 메서드를 만든 뒤 TodoInput 컴포넌트의 props로 전달하는 코드를 작성했습니다.

--- todo-list/src/components/App.tsx
+++ todo-list/src/components/App.tsx
@@ -2,13 +2,31 @@ import React, { Component } from 'react';
 import PageTemplate from './PageTemplate';
 import TodoInput from './TodoInput';
 
+export interface IAppState {
+    input: string
+}
 
-class App extends Component {
+
+class App extends Component<{}, IAppState> {
+    state = {
+        input: ''
+    }
+
+    handleChange = (e:React.SyntheticEvent) => {
+        const { value } = e.target as HTMLInputElement;
+        this.setState({
+            input: value
+        })
+
+    }
+
 
     render() {
+        const {input} = this.state;
+        const {handleChange} = this;
         return (
             <PageTemplate>
-                <TodoInput />
+                <TodoInput onChange={handleChange} value={input} />
             </PageTemplate>
         );
     }
