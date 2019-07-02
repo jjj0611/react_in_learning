a3ffb4d717c809950df84de4015c1c1087c84dfb
jaeju.jang <jjj0611@gmail.com>
2019-07-02 10:23:00 +0900

    [ADD] export가 제대로 이루어지지 않아 해당 코드를 추가하였습니다.

--- todo-list/src/components/TodoList/TodoList.tsx
+++ todo-list/src/components/TodoList/TodoList.tsx
@@ -10,27 +10,30 @@ export interface ITodoListData {
 export interface ITodoListProps {
     todos: Array<ITodoListData>
     onToggle: (id: number) => void
+    onRemove: (id: number) => void
 }
 
 class TodoList extends React.Component<ITodoListProps, {}> {
     render() {
-        const { todos, onToggle } = this.props;
+        const { todos, onToggle, onRemove } = this.props;
         const todoList = todos.map(
             todo => (
                 <TodoItem
                     key={todo.id}
                     done={todo.done}
-                    onToggle={() => onToggle(todo.id)}>
+                    onToggle={() => onToggle(todo.id)}
+                    onRemove={() => onRemove(todo.id)}
+                >
                         {todo.text}
-                </TodoItem>
-                    
+                </TodoItem>           
             )
         );
-
         return (
             <div>
                 {todoList}
             </div>
-        )
+        );
     }
 }
+
+export default TodoList;
